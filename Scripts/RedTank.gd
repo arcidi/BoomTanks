15676fe827085a4f274059d3ea56b57f8569d260
Arcidi <olenderbartlomiej00@gmail.com>
2017-11-05 12:58:14 +0100

    Init

---
+++ Scripts/RedTank.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var speed = 1
+
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
