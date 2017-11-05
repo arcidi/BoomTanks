6ccb5db65fa21c2ecdaa9778647479913fe96683
Arcidi <olenderbartlomiej00@gmail.com>
2017-11-05 22:09:21 +0100

    Basic car movement

--- Scripts/Tank.gd
+++ Scripts/Tank.gd
@@ -1,28 +1,33 @@
 extends KinematicBody2D
 
-var maxSpeed = 15000
+var maxSpeed = 350
 var speed = 0
-var acceleration = 300
-var mass
+var acceleration = 150
 var velocity = Vector2(0,0)
+var stering = 180
 
 
 func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	print(rotation_deg)
-	
 	if Input.is_action_pressed("ui_up"):
-		speed += acceleration
-	if Input.is_action_pressed("ui_down"):
-		speed -= acceleration
+		speed += acceleration * delta
+	elif Input.is_action_pressed("ui_down"):
+		speed -= acceleration * delta
+	else:
+		speed -= speed / 30
 	if Input.is_action_pressed("ui_left"):
-		rotation_deg -= 180 * delta
+		rotation_deg -= stering * inverse_lerp(0, maxSpeed, speed) * delta 
 	if Input.is_action_pressed("ui_right"):
-		rotation_deg += 180 * delta
-	if(speed > maxSpeed):
+		rotation_deg += stering * inverse_lerp(0, maxSpeed, speed) * delta
+	
+	#Clamp doesn't work? Well this is still alfa...
+	if speed > maxSpeed: 
 		speed = maxSpeed
-	velocity = Vector2(sin(-rotation), cos(-rotation)) * speed * delta
-	move_and_slide(velocity)
-	speed -= speed / 100
+	elif speed < -maxSpeed:
+		speed = -maxSpeed
+	
+	velocity = Vector2(sin(-rotation), cos(-rotation)) * speed * delta #Change velocity vector according to rotation
+	move_and_collide(velocity)
+	
