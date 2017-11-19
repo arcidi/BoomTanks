fe73ebf8b000432a575ad707badc2370857f774c
Arcidi <olenderbartlomiej00@gmail.com>
2017-11-19 17:21:22 +0100

    Player inputs

--- Scenes/PlayerTank.gd
+++ Scenes/PlayerTank.gd
@@ -5,18 +5,43 @@ extends KinematicBody2D
 #Input - player sends only float betwen -1 to 1 via rpc
 #Server sends rpc_unreliable position
 
-var velocity = Vector2(0,0)
+
+var velocity = Vector2(0,1)
+var rotation_speed = 1;
 
 
+sync var is_left_pressed = false
+sync var is_right_pressed = false
+sync var is_acc_pressed = false
+sync var is_brake_pressed = false
 
 func _ready():
 	set_physics_process(true)
+	set_process_input(true)
 
 func _physics_process(delta):
 	
-	#Change velocity vector according to rotation
-	velocity = Vector2(sin(-rotation), cos(-rotation)) * speed * delta
 	
-	move_and_collide(velocity)
+	move_and_collide(velocity.rotated(rotation))
+
+
+func _input(event):
+	if event.is_action_pressed("ui_up"):
+		rset("is_acc_pressed", true)
+	if event.is_action_released("ui_up"):
+		rset("is_acc_pressed", false)
 	
-	pass
+	if event.is_action_pressed("ui_down"):
+		rset("is_brake_pressed", true)
+	if event.is_action_released("ui_down"):
+		rset("is_brake_pressed", false)
+	
+	if event.is_action_pressed("ui_left"):
+		rset("is_left_pressed", true)
+	if event.is_action_released("ui_left"):
+		rset("is_left_pressed", false)
+		
+	if event.is_action_pressed("ui_right"):
+		rset("is_right_pressed", true)
+	if event.is_action_released("ui_right"):
+		rset("is_right_pressed", false)
