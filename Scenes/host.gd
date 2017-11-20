c85ad4e53168a3736d1a40b920ff795fcb31792c
kacpx123 <kacpx123@users.noreply.github.com>
2017-11-20 21:20:37 +0100

    Przyciski reszte zrobie w tym tygoniu
    :D

---
+++ Scenes/host.gd
@@ -0,0 +1,9 @@
+extends host_button
+
+func _on_host_button_down():
+	get_tree().change_scene("res://Scenes/main.tscn")
+	pass 
+
+func _on_join_button_down():
+	get.tree().change_scene("res://Scenes/main.tscn")
+	pass
