04e950c21fb4320c1e0f9452ea61ba3a0631c6c5
Arcidi <olenderbartlomiej00@gmail.com>
2017-11-24 17:10:40 +0100

    Working shooting!

--- Scripts/Turret.gd
+++ Scripts/Turret.gd
@@ -1,14 +1,31 @@
 extends Node2D
 
+onready var bullet_scene = preload("res://Scenes/bulletBlue3_outline.tscn")
 
+var shoot_per_seconds = 2
+sync var cool_down_timer = float(1) / shoot_per_seconds
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
+	cool_down_timer -= delta
 	if is_network_master():
 		look_at(get_global_mouse_position())
 		rpc_unreliable("set_rotation", rotation)
+		if Input.is_action_pressed("Fire") && cool_down_timer <= 0:
+			rpc("spawn_bullet")
 
 remote func set_rotation(rot):
 	self.rotation = rot
+
+sync func spawn_bullet():
+	if cool_down_timer <= 0: 
+		var bullet = bullet_scene.instance()
+		if get_tree().is_network_server():
+			bullet.position = get_node("BulletSpawnPoint").global_position
+			bullet.rotation_deg = global_rotation_deg + 90
+			bullet.velocity = Vector2(1,0).rotated(global_rotation)
+			bullet.speed = 500
+		cool_down_timer = float(1) / shoot_per_seconds
+		get_tree().get_root().add_child(bullet)
--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Area2D
 
 var velocity
 var speed
@@ -11,9 +11,11 @@ func _ready():
 func _physics_process(delta):
 	
 	if get_tree().is_network_server():
-		var collision_object = move_and_collide(velocity * speed * delta)
-		rpc_unreliable("update_position", position)
-		print(collision_object.get_name())
+		translate(velocity * speed * delta)
+		rpc_unreliable("update_position", global_transform)
 
-remote func update_position(pos):
-	position = pos
+remote func update_position(trans):
+	global_transform = trans
+
+func _on_bulletBlue3_outline_body_entered( body ):
+	print(body.get_name())
