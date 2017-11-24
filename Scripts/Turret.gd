e89b90fc0ef7556e919cbb2c1a8320e94fad6884
Arcidi <olenderbartlomiej00@gmail.com>
2017-11-24 11:17:01 +0100

    Bullet scene

--- Scripts/Turret.gd
+++ Scripts/Turret.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 
 
-
 func _ready():
 	set_process(true)
 
 func _process(delta):
 	if is_network_master():
 		look_at(get_global_mouse_position())
+		rpc_unreliable("set_rotation", rotation)
+	
+
+remote func set_rotation(rot):
+	self.rotation = rot
